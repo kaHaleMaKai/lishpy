af46c5db5cef0d3efe30ba19e99112fdc50905f8
Lars Winderling <lars.winderling@performance-advertising.de>
2017-06-08 09:05:44 +0200

    Update test_list to current state.

--- test/core/test_list.py
+++ test/core/test_list.py
@@ -1,19 +1,37 @@
 import unittest
-from core.list import *
+from core.list import (List,
+                       EmptyList,
+                       EmptyListWithoutMetaInfo,
+                       EmptyListWithMetaInfo)
 from core.primitives import nil
 
 
 class ListTest(unittest.TestCase):
 
     def setUp(self):
-        self.empty_no_meta = EmptyListWithoutMetaInfo()
+        self.empty_no_meta = EmptyList()
         self.x = List("x", nil, 1)
 
     def test_empty_no_meta(self):
         self.assertIs(self.empty_no_meta, EmptyListWithoutMetaInfo())
-        self.assertIs(self.empty_no_meta, EmptyList())
-        self.assertEqual(EmptyList().conj("x"), self.x)
-        self.assertEqual(EmptyList().conj("x").conj("y"), self.x.conj("y"))
-        self.assertEqual(EmptyList().conj("x", "y"), self.x.conj("y"))
-        self.assertEqual(EmptyList().size(), 0)
-        self.assertEqual(EmptyList().conj("x", "y").size(), 2)
+        self.assertIs(self.empty_no_meta, EmptyListWithMetaInfo())
+        self.assertIs(self.empty_no_meta, List())
+
+    def test_conj(self):
+        self.assertEqual(List().conj("x"), self.x)
+        self.assertEqual(List().conj("x").conj("y"), self.x.conj("y"))
+        self.assertEqual(List().conj("x", "y"), self.x.conj("y"))
+        self.assertEqual(List().size, 0)
+        self.assertEqual(List().conj("x", "y").size, 2)
+
+    def test_properties(self):
+        self.assertEqual(List().size, 0)
+        self.assertEqual(List(meta={"hello": "world"}).size, 0)
+        self.assertEqual(self.x.size, 1)
+        xyz = self.x.conj("y", "z")
+        self.assertEqual(xyz.size, 3)
+        self.assertEqual(xyz.first, "z")
+        self.assertEqual(xyz.next.first, "y")
+        self.assertIs(xyz.next.next, self.x)
+        self.assertEqual(xyz.next.next, self.x)
+
